2b3e41321ca76247930bd76d600728ef46e7a7e2
Rodrigo L <r.lizanaavalos@gmail.com>
2022-01-11 16:51:31 -0300

    se mejoras los test

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -3,19 +3,25 @@ package com.devopsusach2020;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1,1);
 	}
 
 	@Test
 	void contextLoadsTest1() {
+		assertEquals(2,1+1);
 	}
 
 	@Test
 	void contextLoadsTest2() {
+		assertEquals(3,1+2);
 	}
 
 }
